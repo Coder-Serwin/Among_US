b6c0fbde3fefe6faa60a807e53dba181004fcbc4
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-03-26 15:10:42 +0530

    New Changes

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+let credit = document.getElementById("credit");
+
+credit.addEventListener("click", e => {
+    let url = "https://www.youtube.com/channel/UCKE1e1IcGQnpDDzA-cddraw";
+    window.open(url);
+});
