700f7615345b2c9e20fa514dc03670ec0d3fc32d
Coder-Serwin <roughuseforaronserwin@gmail.com>
2022-05-09 07:07:16 +0530

    nges

--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-let credit = document.getElementById("link");
-
-credit.addEventListener("click", e => {
-    let url = "https://www.youtube.com/channel/UCKE1e1IcGQnpDDzA-cddraw";
-    window.open(url);
-});
